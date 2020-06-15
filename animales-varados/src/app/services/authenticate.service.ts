187ea721d2392fd48a5d69a6afd01716267f3bdf
Maor RG <maor.roizman@utec.edu.pe>
2020-06-15 18:20:48 -0500

    Agregando el modelo de usuario y Servicio verifyUser

--- animales-varados/src/app/services/authenticate.service.ts
+++ animales-varados/src/app/services/authenticate.service.ts
@@ -1,32 +1,41 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { HttpClient, HttpHeaders} from '@angular/common/http';
+import { UserInterface } from '../models/user-interface';
 import { Observable } from 'rxjs';
 import { map } from "rxjs/operators";
-
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticateService {
 
-  constructor(
-    private http: HttpClient) {}
+  constructor(private http: HttpClient) {}
 
   httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
   };
+
   
 
   private apiURL = 'http://107.180.91.147:8080/animales_varados-0.1';
 
   /** POST */
-  verifyUser(email: string, password: string):Observable<any>{
-    
+  verifyUser(email: string, password: string){
     const url_api = `${this.apiURL}/authenticate`;
     console.log(url_api);
-    return this.http.post(url_api,{email: email, password: password}).pipe(map(data => data));
+    return this.http.post<UserInterface>(url_api, {email: email, password: password}).subscribe(res => {
+      // RESPONSE ACTION //
+      console.log(res.jwt);
+      alert("Bienvenido!!!");
+    }, error => {
+      // HANDLE ERROR //
+      console.log(error);
+      if(error.status == "500") alert("Datos invalidos");
+    });
   }
+
   
+
 
 
 
